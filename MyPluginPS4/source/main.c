3d9727bfc407b8ae9d46d79a3add3c576f52bd45
TchikaModzz <158184897+TchikaModzz@users.noreply.github.com>
2026-06-24 11:00:25 +0200

    Add files via upload

---
+++ MyPluginPS4/source/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/GoldHEN.h"
+
+// Point d'entrée obligatoire appelé par GoldHEN au chargement du plugin
+int _main(void) {
+    // Message de test (sera visible si vous avez un logger ou un écran de debug)
+    return 0;
+}
